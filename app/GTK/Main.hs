dfa03e8c801d02071de6d923eff987d6303172d6
Chapman Shoop <chapman.shoop@gmail.com>
2024-07-23 21:45:22 -0500

    Add labels for input widgets, organize in a table.

--- app/GTK/Main.hs
+++ app/GTK/Main.hs
@@ -15,25 +15,35 @@ main = do
     window <- windowNew
     window `onDestroy` mainQuit
 
-    vBox <- vBoxNew True 10
+    table <- tableNew 4 2 False
+    tableSetRowSpacings table 10
+    tableSetColSpacings table 10
 
+    sourceLabel <- labelNew (Just "Source Folder:")
     source <- fileChooserWidgetNew FileChooserActionSelectFolder
-    boxPackStart vBox source PackNatural 10
+    widgetSetSizeRequest source 600 300
+    tableAttachDefaults table sourceLabel 0 1 0 1
+    tableAttachDefaults table source 1 2 0 1
 
+    destinationLabel <- labelNew (Just "Destination Folder:")
     destination <- fileChooserWidgetNew FileChooserActionSelectFolder
-    boxPackStart vBox destination PackNatural 10
+    widgetSetSizeRequest destination 600 300
+    tableAttachDefaults table destinationLabel 0 1 1 2
+    tableAttachDefaults table destination 1 2 1 2
 
+    numFilesLabel <- labelNew (Just "Number of Files:")
     numFiles <- spinButtonNewWithRange 1 100 1 
-    boxPackStart vBox numFiles PackNatural 10
+    tableAttachDefaults table numFilesLabel 0 1 2 3
+    tableAttachDefaults table numFiles 1 2 2 3
 
     copy <- buttonNewWithLabel "Copy Random Files"
     copy `onClicked` (do n <- spinButtonGetValueAsInt numFiles
                          s <- fileChooserGetFilename source
                          d <- fileChooserGetFilename destination
                          copyRandomFiles n (fromJust s) ((fromJust d) ++ "/"))
-    boxPackStart vBox copy PackNatural 10
+    tableAttachDefaults table copy 0 2 3 4
     
-    containerAdd window vBox
+    containerAdd window table
     widgetShowAll window
 
     mainGUI
